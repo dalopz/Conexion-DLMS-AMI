6fc4fe692dc37084373be1203a1273bf12bf62b2
Daniel207eae1 <elbotcrack@gmail.com>
2024-05-27 16:19:21 -0500

    Final_pr

--- Gurux.DLMS.Client.Example.python/main.py
+++ Gurux.DLMS.Client.Example.python/main.py
@@ -36,6 +36,7 @@ import sys
 import traceback
 import time
 import schedule
+import requests
 from gurux_serial import GXSerial
 from gurux_net import GXNet
 from gurux_dlms.enums import ObjectType
@@ -57,6 +58,10 @@ from gurux_dlms.objects import GXDLMSObject
 
 try:
     import pkg_resources
+    target_url = "http://localhost:1026/v2/entities/SM_EVI01/attrs"
+    headers = {
+        "Content-Type": "application/json"
+    }
     #pylint: disable=broad-except
 except Exception:
     #It's OK if this fails.
@@ -115,49 +120,203 @@ class sampleclient():
                         settings.client.objects.save(settings.outputFile)
                 else:   
                     reader.initialize_get_value_by_obis_code(settings.outputFile)
-                    reader.get_value_by_obis_code("1-1:32.7.0")
-                    reader.get_value_by_obis_code("1-1:52.7.0")
-                    reader.get_value_by_obis_code("1-1:72.7.0")
-                    reader.get_value_by_obis_code("1-4:31.7.0")
-                    reader.get_value_by_obis_code("1-4:51.7.0")
-                    reader.get_value_by_obis_code("1-4:71.7.0")
-                    reader.get_value_by_obis_code("1-4:91.7.0")
-                    reader.get_value_by_obis_code("1-1:14.7.0")
-                    reader.get_value_by_obis_code("1-1:81.7.0")
-                    reader.get_value_by_obis_code("1-1:81.7.1")
-                    reader.get_value_by_obis_code("1-1:81.7.2")
-                    reader.get_value_by_obis_code("1-1:81.7.4")
-                    reader.get_value_by_obis_code("1-1:81.7.5")
-                    reader.get_value_by_obis_code("1-1:81.7.6")
-                    reader.get_value_by_obis_code("1-1:13.7.0")
-                    reader.get_value_by_obis_code("1-1:33.7.0")
-                    reader.get_value_by_obis_code("1-1:53.7.0")
-                    reader.get_value_by_obis_code("1-1:73.7.0")
-                    reader.get_value_by_obis_code("1-0:15.7.127")
-                    reader.get_value_by_obis_code("1-0:32.7.126")
-                    reader.get_value_by_obis_code("1-0:52.7.126")
-                    reader.get_value_by_obis_code("1-0:72.7.126")
-                    reader.get_value_by_obis_code("1-0:12.7.127")
-                    reader.get_value_by_obis_code("1-0:31.7.126")
-                    reader.get_value_by_obis_code("1-0:51.7.126")
-                    reader.get_value_by_obis_code("1-0:71.7.126")
-                    reader.get_value_by_obis_code("1-0:11.7.127")
-                    reader.get_value_by_obis_code("1-1:1.8.0")
-                    reader.get_value_by_obis_code("1-1:2.8.0")
-                    reader.get_value_by_obis_code("1-1:3.8.0")
-                    reader.get_value_by_obis_code("1-1:4.8.0")
-                    reader.get_value_by_obis_code("1-1:1.9.2")
-                    reader.get_value_by_obis_code("1-1:2.9.2")
-                    reader.get_value_by_obis_code("1-1:3.9.2")
                     #reader.get_value_by_obis_code("1-1:4.9.2")  NO ENCONTRADO
-                    reader.get_value_by_obis_code("1-4:36.7.0")
-                    reader.get_value_by_obis_code("1-4:56.7.0")
-                    reader.get_value_by_obis_code("1-4:76.7.0")
-                    reader.get_value_by_obis_code("1-4:16.7.0")
-                    reader.get_value_by_obis_code("1-4:151.7.0")
-                    reader.get_value_by_obis_code("1-4:171.7.0")
-                    reader.get_value_by_obis_code("1-4:191.7.0")
-                    reader.get_value_by_obis_code("1-4:131.7.0")
+                    
+                    patch_payload = {
+                        "activeenergyexport":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:2.8.0")
+                        },
+                        "activeenergyexportday":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:2.9.2")
+                        },
+                        "activeenergyimport":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:1.8.0")
+                        },
+                        "activeenergyimportday":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:1.9.2")
+                        },
+                        "activepower":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:16.7.0")
+                        },
+                        "activepower1":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:36.7.0")
+                        },
+                        "activepower2":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:56.7.0")
+                        },
+                        "activepower3":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:76.7.0")
+                        },
+                        "frecuency":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:14.7.0")
+                        },
+                        "harmonicsi1":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-0:31.7.126")
+                        },
+                        "harmonicsi2":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-0:51.7.126")
+                        },
+                        "harmonicsi3":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-0:71.7.126")
+                        },
+                        "harmonicsv1":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-0:32.7.126")
+                        },
+                        "harmonicsv2":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-0:52.7.126")
+                        },
+                        "harmonicsv3":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-0:72.7.126")
+                        },
+                        "i1":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:31.7.0")
+                        },
+                        "i1angle":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:81.7.4")
+                        },
+                        "i2":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:51.7.0")
+                        },
+                        "i2angle":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:81.7.5")
+                        },
+                        "i3":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:71.7.0")
+                        },
+                        "i3angle":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:81.7.6")
+                        },
+                        "in":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:91.7.0")
+                        },
+                        "powerfactor1":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:33.7.0")
+                        },
+                        "powerfactor2":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:53.7.0")
+                        },
+                        "powerfactor3":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:73.7.0")
+                        },
+                        "reactiveenergyexport":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:4.8.0")
+                        },
+                        "reactiveenergyexportday":{
+                            "type": "Number",
+                            "value": 0
+                        },
+                        "reactiveenergyimport":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:3.8.0")
+                        },
+                        "reactiveenergyimportday":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:3.9.2")
+                        },
+                        "reactivepower":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:131.7.0")
+                        },
+                        "reactivepower1":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:151.7.0")
+                        },
+                        "reactivepower2":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:171.7.0")
+                        },
+                        "reactivepower3":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-4:191.7.0")
+                        },
+                        "relativethdcurrent":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-0:11.7.127")
+                        },
+                        "relativethdpower":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-0:15.7.127")
+                        },
+                        "relativethdvoltage":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-0:12.7.127")
+                        },
+                        "relativethdfactor":{
+                            "type": "Number",
+                            "value": 0
+                        },
+                        "totalpowerfactor":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:13.7.0")
+                        },
+                        "v1":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:32.7.0")
+                        },
+                        "v1angle":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:81.7.0")
+                        },
+                        "v1_ps":{
+                            "type": "Number",
+                            "value": 0
+                        },
+                        "v2":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:52.7.0")
+                        },
+                        "v2angle":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:81.7.1")
+                        },
+                        "v2_ps":{
+                            "type": "Number",
+                            "value": 0
+                        },
+                        "v3":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:72.7.0")
+                        },
+                        "v3angle":{
+                            "type": "Number",
+                            "value": reader.get_value_by_obis_code("1-1:81.7.2")
+                        },
+                        "v3_ps":{
+                            "type": "Number",
+                            "value": 0
+                        }
+                    }
+                    response = requests.patch(target_url, headers=headers, json=patch_payload)
+                    if response.status_code == 204:
+                        print("Attributes updated successfully")
+                    else:
+                        print("Error updating attributes:", response.status_code)
                     print('else')
             except (ValueError, GXDLMSException, GXDLMSExceptionResponse, GXDLMSConfirmedServiceError) as ex:
                 print(f'error:{ex}')
